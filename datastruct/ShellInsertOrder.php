8984f3a1a85145fc12846ab062f82162985ae0f1
Joe Chan <cshujun21625@163.com>
2018-09-28 10:25:06 +0800

    update alogrithm 'for' to 'while' try in win7 not faster than d.sort

--- datastruct/ShellInsertOrder.php
+++ datastruct/ShellInsertOrder.php
@@ -25,28 +25,30 @@ class ShellInsertOrder implements sort
         $i = 0;
         $j = 0;
         $k = 0;
-        $dt = [403, 27, 1];
-        for ($k = 0; $k < count($dt); $k++)
-            for ($i = $dt[$k] + 1; $i < count($input); $i++)
-            {
+        $dt = [3037,502,19,1];
+        for ($k = 0; $k< count($dt); $k++)
+            for($i = $dt[$k] + 1; $i < count($input); $i++)
                 if ($input[$i] < $input[$i - $dt[$k]])
                 {
-                    $input[0] =  $input[$i];
-                    for ($j = $i - $dt[$k]; $j > 0 && $input[0] < $input[$j]; $j = $j - $dt[$k])
+                    $input[0] = $input[$i];
+                    $j = $i - $dt[$k];
+                    while ($j > 0 && $input[0] < $input[$j])
+                    {
                         $input[$j + $dt[$k]] = $input[$j];
+                        $j = $j -$dt[$k];
+                    }
                     $input[$j + $dt[$k]] = $input[0];
                 }
-            }
         $end = microtime();
         
         list($msec, $sec) =  explode(" ", $start);
         list($msec1, $sec1) =  explode(" ", $end);
         if (intval($_SESSION['execution']['times']) == 0)
-            echo("<br />+--------------execution time--------------+<br />");
+            echo("\n+--------------execution time--------------+\n");
         $mdist =  doubleval($msec1) - doubleval($msec);
         $sdist =  doubleval($sec1)  - doubleval($sec);
         echo $sdist + $mdist;   
-        echo("s<br />");
+        echo("s\n");
         
         $this->store($sdist + $mdist);
     }

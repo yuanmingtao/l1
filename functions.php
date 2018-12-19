755acaf6dcbd3465d85801702941076c2c842647
'Joe.Chan' <cshujun21625@163.com>
2018-12-19 10:51:15 +0800

    initial function practice

---
+++ functions.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is practice functions.
+ * @author Joe.Chan
+ *  2018-12-19 10:20 $
+*/
+
+function foo ($name)
+{
+    class A{
+        function __construct($name)
+        {
+            echo "welcome $name".PHP_EOL;
+        }
+        
+        function __toString()
+        {
+            return  "A";
+        }
+        
+        function test()
+        {
+            echo "test".PHP_EOL;
+        }
+    }
+    
+    return new A($name);
+}
+
+
+$t = foo("Joe");
+$t->test();

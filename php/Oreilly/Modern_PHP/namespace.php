e7b408f6c0e8156037aa43e042054075ffc7cedb
Joe.Chan <cshujun21625@gmail.com>
2019-07-11 09:27:26 +0800

    Modern PHP start

---
+++ php/Oreilly/Modern_PHP/namespace.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Just for learning not commerical
+ *
+ * @author    Joe.Chan<cshujun21625@gmail.com>
+ * @date      2019年7月10日 下午5:18:43
+ * @link      git@github.com:yuanmingtao/l1.git
+ */
+
+
+require 'preface.php';
+use function Oreilly\ModernPHP\china;
+use const Oreilly\ModernPHP\OK;
+china();
+echo OK;
---
+++ php/Oreilly/Modern_PHP/preface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Just for learning not commerical
+ * 
+ * Modern PHP
+ * 
+ * @author    Joe.Chan<cshujun21625@gmail.com>
+ * @date      2019年7月5日 下午3:28:01
+ * @link      git@github.com:yuanmingtao/l1.git
+ */
+namespace Oreilly\ModernPHP;
+const OK = "I am ok.";
+function china()
+{
+    echo "china" . PHP_EOL;
+}

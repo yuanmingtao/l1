9ec6ea18ffd60de4e4392c1f3011ddb2d2d39bbe
Joe.Chan <cshujun21625@gmail.com>
2019-09-11 16:28:59 +0800

    last learn

---
+++ php/fundamental/pcre.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Just for learning not commerical
+ *
+ * @author    Joe.Chan<cshujun21625@gmail.com>
+ * @date      2019年8月12日 下午2:18:43
+ * @link      git@github.com:yuanmingtao/l1.git
+ */
---
+++ php/info.php
@@ -0,0 +1,4 @@
+<?php
+
+
+var_dump(password_verify("888888", '$2y$15$xKHgOOfeBJQ6RRfy4voaD.ofoEtF6JdxECJdPrMRhNTseQHvMIAA.'));
